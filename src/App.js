c374bf93e0b4be9763cd6ba73c535b1ad92a9444
Dylan <dylan.sieren01@gmail.com>
2021-06-24 10:35:04 -0500

    missed some redirect components.. oops

--- src/App.js
+++ src/App.js
@@ -29,7 +29,8 @@ function App() {
            {/* <Route exact path="/signup" render={props => <SignupForm {...props}/> } />
             <Route exact path="/signup/interests" component={InterestListPage} />
             <Route exact path="/signup/add-profile"  component={ProfileCreationPage} /> */}
-            <Redirect  component={Page404} />
+            <Route path="/404" component={Page404} />
+            <Redirect to="/404" />
           </Switch>
         </div>
     </Router>
