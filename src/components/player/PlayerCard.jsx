44b2c68391c8501e451e17c28c8ec6a3970aed95
Dylan <dylan.sieren01@gmail.com>
2021-07-04 22:31:27 -0600

    added discord user_id for avatar img

--- src/components/player/PlayerCard.jsx
+++ src/components/player/PlayerCard.jsx
@@ -5,6 +5,7 @@ import { roleArr } from '../pages/RoleImageExport'
 
 export function Playercard(props) {
     const {listing} = props;
+    console.log(listing);
 
     // Filter our the rank file
     let rank = rankedEmblemArr.filter(rank => rank.name == listing.rank)
@@ -14,8 +15,10 @@ export function Playercard(props) {
     console.log(listing.roles)
     return(
         <ListingCard>
-            <CardAvatar src={listing.avatar}/>
-            <CardUsername>{`${listing.username}#${listing.discriminator}`}</CardUsername>
+            <UsernameContainer>
+                <CardAvatar src={`https://cdn.discordapp.com/avatars/${listing.discord_id}/${listing.avatar}.png`}/>
+                <CardUsername>{`${listing.username}`}</CardUsername>                
+            </UsernameContainer>
             <CardRank src={rank}/>
             <RoleContainer>
                 {roleArr.map( role => {
@@ -35,37 +38,44 @@ export function Playercard(props) {
 export default Playercard
 
 const ListingCard = S.div`
-    flex: 1;
     margin-right: 15px;
     border-radius: 15px;
     padding: 15px;
     display: flex;
     flex-flow: row wrap;
     max-width: 250px;
+    justify-content: center;
+`;
+const UsernameContainer = S.div`
+    width: 100%;
+    display: flex;
+    flex-flow: row wrap;
+    align-items: center;
 `;
 const CardUsername = S.p`
     font-size: 20px;
     color: #fff;
     font-weight: 500;
-    width: 100%;
     text-align: right;
+    margin-left: 20px;
 `;
 const CardAvatar = S.img`
-    width: 25px;
-    height: 25px;
+    width: 65px;
+    height: 65px;
     border-radius: 50%;
-    float: left;
 `;
 const CardRank = S.img`
-    width: 100px;
+    width: 175px;
     height: auto;
 `;
 const RoleContainer = S.div`
     display: flex;
     flex-flow: row wrap;
     width: 100%;
+    justify-content: center;
 `;
 const CardRole = S.img`
     width: 50px;
     height: auto;
+    margin: 20px;
 `;
