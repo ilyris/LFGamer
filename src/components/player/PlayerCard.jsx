bde74bba98ec928800b72efcbdc782f4665c267d
Dylan <dylan.sieren01@gmail.com>
2021-07-05 20:42:22 -0600

    formatted and styled the duo listing card a bit more, still lacking the description about the post

--- src/components/player/PlayerCard.jsx
+++ src/components/player/PlayerCard.jsx
@@ -17,21 +17,50 @@ export function Playercard(props) {
         <ListingCard>
             <UsernameContainer>
                 <CardAvatar src={`https://cdn.discordapp.com/avatars/${listing.discord_id}/${listing.avatar}.png`}/>
-                <CardUsername>{`${listing.username}`}</CardUsername>                
+                <CardUsername>{`${listing.username}#${listing.discriminator}`}</CardUsername>                
             </UsernameContainer>
-            <CardRank src={rank}/>
-            <RoleContainer>
-                {roleArr.map( role => {
-                    let doesContain = false;
-                    listing.roles.map(userRole => {
-                        if(role.name == userRole) doesContain = true;
-                    }) 
-                    if(doesContain) {
-                        return <CardRole src={role.file}/>
-                    }
-                })}
-            </RoleContainer>
-            
+            <InformationContainer>
+                <Label> Rank:
+                    <CardRank src={rank}/>
+                </Label>
+                <Label> Roles:
+                <RoleContainer>
+                    {roleArr.map( role => {
+                        let doesContain = false;
+                        listing.roles.map(userRole => {
+                            if(role.name == userRole) doesContain = true;
+                        }) 
+                        if(doesContain) {
+                            return <CardRole src={role.file}/>
+                        }
+                    })}
+                </RoleContainer>
+                </Label>
+            </InformationContainer>
+            <InformationContainer>
+                <Label> Champions
+                    <ChampionContainer>
+                        <ChampionImg src={`${process.env.PUBLIC_URL}/assets/riot_games_champion_images/Aatrox.png`}/>
+                        <ChampionImg src={`${process.env.PUBLIC_URL}/assets/riot_games_champion_images/Aatrox.png`}/>
+                        <ChampionImg src={`${process.env.PUBLIC_URL}/assets/riot_games_champion_images/Aatrox.png`}/>
+                        <ChampionImg src={`${process.env.PUBLIC_URL}/assets/riot_games_champion_images/Aatrox.png`}/>
+
+                    </ChampionContainer>
+                </Label>
+                <Label> Looking for:
+                <RoleContainer>
+                    {roleArr.map( role => {
+                        let doesContain = false;
+                        listing.roles.map(userRole => {
+                            if(role.name == userRole) doesContain = true;
+                        }) 
+                        if(doesContain) {
+                            return <CardRole src={role.file}/>
+                        }
+                    })}
+                </RoleContainer>
+                </Label>
+            </InformationContainer>
         </ListingCard>
     )
 }
@@ -44,13 +73,17 @@ const ListingCard = S.div`
     display: flex;
     flex-flow: row wrap;
     max-width: 250px;
-    justify-content: center;
+    justify-content: space-between;
+    background-color: #232323;
+    border-radius: 10px;
+    box-shadow: 2px 2px 8px #00000061;
 `;
 const UsernameContainer = S.div`
     width: 100%;
     display: flex;
     flex-flow: row wrap;
     align-items: center;
+    justify-content: center;
 `;
 const CardUsername = S.p`
     font-size: 20px;
@@ -60,22 +93,43 @@ const CardUsername = S.p`
     margin-left: 20px;
 `;
 const CardAvatar = S.img`
-    width: 65px;
-    height: 65px;
+    width: 50px;
+    height: 50px;
     border-radius: 50%;
 `;
 const CardRank = S.img`
-    width: 175px;
+    width: 100px;
     height: auto;
+    margin-top: 10px;
+
 `;
 const RoleContainer = S.div`
-    display: flex;
-    flex-flow: row wrap;
-    width: 100%;
-    justify-content: center;
+    margin-top: 10px;
+
 `;
 const CardRole = S.img`
-    width: 50px;
+    width: 35px;
     height: auto;
-    margin: 20px;
+`;
+const InformationContainer = S.div`
+    display: flex;
+    width: 100%;
+    margin-top: 20px;
+`;
+const Label = S.label`
+    font-size: 14px;
+    color: gray;
+    width: 49%;
+`;
+const ChampionContainer = S.div`
+    flex-flow: row wrap;
+    justify-content: center;
+    width: 100%;
+    display: flex;
+    margin-top: 10px;
+`;
+const ChampionImg = S.img`
+    border-radius: 50%;
+    margin: 5px;
+    max-width: 40px;
 `;
