b7437f34c2b97413bb75237c76ef78672d1fe8a9
Dylan <dylan.sieren01@gmail.com>
2021-07-06 00:42:01 -0600

    updated the player card, the file is too big, needs to be broken into smaller components

---
+++ src/components/pageComponents/PrimaryCtaLink.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import S from 'styled-components';
+import {Link} from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
+export function PrimaryCtaLink(props) {
+    
+    return (
+        <Button>{props.text}
+            <StyledIconArrow icon={faArrowRight} />  
+        </Button>
+    )
+}
+const StyledIconArrow = S(FontAwesomeIcon)`
+    transform: translateX(-100%);
+    color: #fff;
+    font-size: 18px;
+    position: absolute;
+    opacity: 0;
+    transition: all ease-in-out 300ms;
+    margin-left: 8px;
+    margin-top: 5px;
+    color: #191818;
+`;
+
+const Button = S(Link)`
+    padding: 10px 30px;
+    border-radius: 40px;
+    color: #fff;
+    font-size: 20px;
+    position: relative;
+    transition: all ease 300ms;
+    background: linear-gradient(to right,rgba(118,238,116,1) 0%,rgba(0,152,142,1) 100%);
+    box-shadow:  2px 2px 12px 0px #494848c4;
+    border: none;
+    z-index: 10;
+    &:hover {
+        padding-right: 50px;
+        cursor: pointer;
+        ${StyledIconArrow} {
+            transform: translateX(0);
+            opacity: 1;
+        }
+    }
+`;
--- src/components/player/PlayerCard.jsx
+++ src/components/player/PlayerCard.jsx
@@ -2,81 +2,121 @@ import React from 'react'
 import S from 'styled-components';
 import { rankedEmblemArr } from '../pages/RankImageExport'
 import { roleArr } from '../pages/RoleImageExport'
+import {PrimaryCtaLink} from '../pageComponents/PrimaryCtaLink';
 
 export function Playercard(props) {
     const {listing} = props;
-    console.log(listing);
-
+    console.log(listing)
     // Filter our the rank file
     let rank = rankedEmblemArr.filter(rank => rank.name == listing.rank)
     rank = rank[0].file;
 
-    // Filter out the roles
-    console.log(listing.roles)
     return(
-        <ListingCard>
-            <UsernameContainer>
-                <CardAvatar src={`https://cdn.discordapp.com/avatars/${listing.discord_id}/${listing.avatar}.png`}/>
-                <CardUsername>{`${listing.username}#${listing.discriminator}`}</CardUsername>                
-            </UsernameContainer>
-            <InformationContainer>
-                <Label> Rank:
-                    <CardRank src={rank}/>
-                </Label>
-                <Label> Roles:
-                <RoleContainer>
-                    {roleArr.map( role => {
-                        let doesContain = false;
-                        listing.roles.map(userRole => {
-                            if(role.name == userRole) doesContain = true;
-                        }) 
-                        if(doesContain) {
-                            return <CardRole src={role.file}/>
-                        }
-                    })}
-                </RoleContainer>
-                </Label>
-            </InformationContainer>
-            <InformationContainer>
-                <Label> Champions
-                    <ChampionContainer>
-                        <ChampionImg src={`${process.env.PUBLIC_URL}/assets/riot_games_champion_images/Aatrox.png`}/>
-                        <ChampionImg src={`${process.env.PUBLIC_URL}/assets/riot_games_champion_images/Aatrox.png`}/>
-                        <ChampionImg src={`${process.env.PUBLIC_URL}/assets/riot_games_champion_images/Aatrox.png`}/>
-                        <ChampionImg src={`${process.env.PUBLIC_URL}/assets/riot_games_champion_images/Aatrox.png`}/>
-
-                    </ChampionContainer>
-                </Label>
-                <Label> Looking for:
-                <RoleContainer>
-                    {roleArr.map( role => {
-                        let doesContain = false;
-                        listing.roles.map(userRole => {
-                            if(role.name == userRole) doesContain = true;
-                        }) 
-                        if(doesContain) {
-                            return <CardRole src={role.file}/>
-                        }
-                    })}
-                </RoleContainer>
-                </Label>
-            </InformationContainer>
-        </ListingCard>
+        <PesudoContainer>
+    <ListingCard >
+                <UsernameContainer>
+                    <CardAvatar src={`https://cdn.discordapp.com/avatars/${listing.discord_id}/${listing.avatar}.png`}/>
+                    <CardUsername>{`${listing.username}#${listing.discriminator}`}</CardUsername>                
+                </UsernameContainer>
+                <LeftColumn>
+                    <InformationContainer>
+                        <LabelContainer> 
+                            <LabelContainerText>Rank:</LabelContainerText>
+                            <CardRank src={rank}/>
+                        </LabelContainer>
+                        <LabelContainer>
+                            <LabelContainerText>Roles:</LabelContainerText>
+                        <RoleContainer>
+                            {roleArr.map( role => {
+                                let doesContain = false;
+                                listing.roles.map(userRole => {
+                                    if(role.name == userRole) doesContain = true;
+                                }) 
+                                if(doesContain) {
+                                    return <CardRole src={role.file}/>
+                                }
+                            })}
+                        </RoleContainer>
+                        </LabelContainer>
+                    </InformationContainer>
+                    <InformationContainer>
+                        <LabelContainer> 
+                            <LabelContainerText>Champions</LabelContainerText>
+                            <ChampionContainer>
+                                {listing.champions.map(champion => {
+                                    return <ChampionImg src={`${process.env.PUBLIC_URL}/assets/riot_games_champion_images/${champion}.png`}/>
+                                })}
+                            </ChampionContainer>
+                        </LabelContainer>
+                        <LabelContainer> 
+                            <LabelContainerText>Looking for:</LabelContainerText>
+                        <RoleContainer>
+                            {roleArr.map( role => {
+                                let doesContain = false;
+                                listing.roles.map(userRole => {
+                                    if(role.name == userRole) doesContain = true;
+                                }) 
+                                if(doesContain) {
+                                    return <CardRole src={role.file}/>
+                                }
+                            })}
+                        </RoleContainer>
+                        </LabelContainer>
+                    </InformationContainer>                
+                </LeftColumn>
+                <Container>
+                    <LabelContainer backSide={true}>
+                        <LabelContainerText>Description</LabelContainerText>
+                        <Text>Some long text about how I am some kind of mid laner</Text>
+                    </LabelContainer>
+                </Container>
+            </ListingCard>
+            <ButtonContainer>
+                <PrimaryCtaLink text={'Message'}/>
+            </ButtonContainer>
+        </PesudoContainer>
+        
     )
 }
 export default Playercard
-
+const Container = S.div`
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    background-color: #232323;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    border-radius: 10px;
+    padding: 15px;
+    transform: translateY(100%);
+    transition: 400ms all ease;
+    opacity: 0;
+`;
+const PesudoContainer = S.div`
+    position: relative;
+`;
 const ListingCard = S.div`
     margin-right: 15px;
     border-radius: 15px;
-    padding: 15px;
+    padding: 15px 15px 35px 15px;
     display: flex;
     flex-flow: row wrap;
-    max-width: 250px;
+    max-width: 350px;
     justify-content: space-between;
     background-color: #232323;
     border-radius: 10px;
     box-shadow: 2px 2px 8px #00000061;
+    position: relative;
+    overflow: hidden;
+    &:hover {
+        ${Container} {
+          transform:translateY(0);
+          z-index: 10;
+        opacity: 1;
+        }
+    }
 `;
 const UsernameContainer = S.div`
     width: 100%;
@@ -97,6 +137,9 @@ const CardAvatar = S.img`
     height: 50px;
     border-radius: 50%;
 `;
+const LeftColumn = S.div`
+    flex: 1;
+`;
 const CardRank = S.img`
     width: 100px;
     height: auto;
@@ -116,10 +159,12 @@ const InformationContainer = S.div`
     width: 100%;
     margin-top: 20px;
 `;
-const Label = S.label`
+const LabelContainer = S.div`
+    width: ${props => props.backSide ? '100%': '49%' };
+`;
+const LabelContainerText = S.p`
     font-size: 14px;
     color: gray;
-    width: 49%;
 `;
 const ChampionContainer = S.div`
     flex-flow: row wrap;
@@ -132,4 +177,18 @@ const ChampionImg = S.img`
     border-radius: 50%;
     margin: 5px;
     max-width: 40px;
+`;
+const Text = S.p`
+    font-size: 18px;
+    color: #fff;
+    margin-top: 10px;
+`;
+const ButtonContainer = S.div`
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    position: absolute;
+    left: 0;
+    bottom: -26px;
+    z-index: 1000;
 `;
