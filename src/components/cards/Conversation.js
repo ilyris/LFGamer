5c3b03e6fdce85a14beea557f9f6746e38af9272
Dylan <dylan.sieren01@gmail.com>
2021-07-16 13:25:20 -0600

    forgot to remove the static user_id for the conversation cards

--- src/components/cards/Conversation.js
+++ src/components/cards/Conversation.js
@@ -24,7 +24,7 @@ import {env_be_url} from '../../globalVars/envURL';
       console.log(c)
       let friendId = c.members.filter(id => id !== loggedInUserId)
       try {
-        const res = await axios.post(`${env_be_url}login/user`, {user_id: 1});
+        const res = await axios.post(`${env_be_url}login/user`, {user_id: friendId[0]});
         setUser(res.data);
         console.log(res);
       } catch(err) {
